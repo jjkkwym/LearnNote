d07dc25dd802f1a7fc647996cdbc40dd5de12d1c
jjkkwym <jjkkwym@outlook.com>
2022-09-14 10:02:27 +0800

    add cmd parser

--- script/parse_config.py
+++ script/parse_config.py
@@ -16,15 +16,37 @@ data_parse = json.loads(data)
 
 class parse_config(object):
     def __init__(self) -> None:
+        self.update_config()
+
+    def update_config(self):
         with open('config.json','r') as f:
             data = f.read()
             f.close()
 
-        data_parse = json.loads(data)
-        print(data_parse)
-        self.target_addr = data_parse['target_addr']
-        self.serial_setting = data_parse['serial_setting']
+        self.data_parse = json.loads(data)
+        self.target_addr = self.data_parse['target_addr']
+        self.serial_setting = self.data_parse['serial_setting']
         self.baudrate = self.serial_setting['baudrate']
+        self.cmd_list_size = len(self.data_parse['cmd_list'])
+        self.cmd_list = self.data_parse['cmd_list']
         #print(self.target_addr)
-    # def target_addr(self):
+    def print_parse(self):
+        print(self.data_parse)
+        cmd_list_size = len(self.data_parse['cmd_list'])
+        for i in range(cmd_list_size):
+            print(self.data_parse['cmd_list'][i]['cmd'])                    
+            print(self.data_parse['cmd_list'][i]['params'])                    
         # return self.target_addr
+
+    def search_cmd_list(self,cmd):
+        for i in range(self.cmd_list_size):
+            print(self.data_parse['cmd_list'][i]['cmd'])                    
+            print(self.data_parse['cmd_list'][i]['params'])                    
+            if cmd == data_parse['cmd_list'][i]['cmd']:
+                return data_parse['cmd_list'][i]['cmd'],data_parse['cmd_list'][i]['params']
+        return None
+
+if __name__ == '__main__':
+    config = parse_config()
+    config.print_parse()
+
--- script/serial_tool.py
+++ script/serial_tool.py
@@ -6,19 +6,21 @@ import threading
 import time
 import datetime
 import parse_config
+import socket
+from prompt_toolkit import prompt
+from prompt_toolkit.history import FileHistory
 
-
-
-class serial_tool(serial.Serial):
+class Serial_tool(serial.Serial):
     
     def __init__(self) -> None:
         super().__init__()
-        self.baudrate = 115200
+        self.config = parse_config.parse_config()
+        self.baudrate = self.config.baudrate
         self.timeout = 0
         self.port = '/dev/ttyUSB0'
         self.recv_thread = None
-        self.is_alive = None
-        #self.open_port()
+        self.is_alive = False
+        self.recv_data_output_hanler = print
     def open_port(self):        
         self.open()
         self.recv_thread = threading.Thread(target=self.recv_handler, name='recv')
@@ -36,8 +38,12 @@ class serial_tool(serial.Serial):
         unhandle_data = ''
         unhandle_data_timeout_count = 0
         while self.is_alive:
-            data_in_waiting = self.in_waiting
-            if self.in_waiting:
+            try:
+                data_in_waiting = self.in_waiting
+            except OSError:
+                data_in_waiting = 0
+            
+            if  data_in_waiting:
                 data = self.read(1024)
                 if data:
                     timestamp = datetime.datetime.now().strftime('[%Y-%m-%d %H:%M:%S.%f]')
@@ -56,25 +62,80 @@ class serial_tool(serial.Serial):
                     split_data_len = len(split_data)
                     #print('split data: ',split_data)
                     for i in range(split_data_len - 1):
-                        timestamp = datetime.datetime.now().strftime('[%Y-%m-%d %H:%M:%S.%f]')
-                        
-                        self.at_cmd_handler(split_data[i])                        
-                        print(timestamp,split_data[i])
+                        output_data = self.get_timestamp() + split_data[i]
+                        #self.at_cmd_handler(split_data[i])                        
+                        self.recv_data_output_hanler(output_data)
                     if split_data[-1] == '':
                         unhandle_data = ''
                         #print('revc data end with CR LF')
                     else:
                         unhandle_data = split_data[-1]
-                        unhandle_data_timeout_count = 5
+                        unhandle_data_timeout_count = 3
             else:
                 if unhandle_data != '':
-                    timestamp = datetime.datetime.now().strftime('[%Y-%m-%d %H:%M:%S.%f]')
                     unhandle_data_timeout_count -= 1
-                    print(timestamp,'unhandle_data_timeout_count:',unhandle_data_timeout_count)
+                    #print(timestamp,'unhandle_data_timeout_count:',unhandle_data_timeout_count)
                     if unhandle_data_timeout_count == 0:
-                        print(timestamp,'unhandle_data timeout,data:',unhandle_data)
+                        output_data = self.get_timestamp() + unhandle_data
+                        print(timestamp,'unhandle_data_timeout_count:',unhandle_data_timeout_count)
+                        self.recv_data_output_hanler(output_data)
                         unhandle_data = ''
             time.sleep(0.01)
+    
+    def register_recv_data_output_hanler(self,handler=print):
+        self.recv_data_output_hanler = handler 
+    
+    def get_timestamp(self):
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+        timestamp = timestamp[0:-3]
+        timestamp = '[' + timestamp + '] '
+        return timestamp
+    
+    def hanle_at_cmd_send(self,opcode):
+        self.config.update_config()
+        if opcode == '1':
+            ser_tool.list_port()
+        elif opcode == '2':
+            ser_tool.open_port()
+        elif opcode == '3':
+            ser_tool.close_port()
+        elif opcode == '4':
+            ser_tool.write('AT+B GPRL\r\n'.encode('utf-8'))
+        elif opcode == '5':
+            ser_tool.send_at_cmd('INQU','2')
+        elif opcode == '6':
+            ser_tool.send_at_cmd('A2DPCONN',ser_tool.config.target_addr)
+        elif opcode == 'CONN':
+            ser_tool.send_at_cmd('A2DPCONN',ser_tool.config.target_addr)
+        elif opcode == 'DISC':
+            ser_tool.send_at_cmd('A2DPDISC')
+        elif opcode == 'START':
+            ser_tool.send_at_cmd('A2DPSTART')
+        elif opcode == 'SUSPEND':
+            ser_tool.send_at_cmd('A2DPSUSPEND')
+        elif opcode == 'CLOSE':
+            ser_tool.send_at_cmd('A2DPCLOSE')
+        elif opcode == 'GLBD':
+            ser_tool.send_at_cmd('GLBD')
+        elif opcode == 'GPRL':
+            ser_tool.send_at_cmd('GPRL')
+        elif opcode == 'DPRD':
+            ser_tool.send_at_cmd('DPRD','00000000000000')
+        elif opcode == 'INQU':
+            ser_tool.send_at_cmd('INQU','2')
+        elif opcode == 'AGCONN':
+            ser_tool.send_at_cmd('AGCONN',ser_tool.config.target_addr)
+        elif opcode == 'AGDISC':    
+            ser_tool.send_at_cmd('AGDISC')
+        else:
+            opcode_split = opcode.split(' ')
+            if len(opcode_split) == 2:
+                ser_tool.send_at_cmd(opcode_split[0],opcode_split[1])
+            else:
+                for cmd in range():
+                    None
+                ser_tool.send_at_cmd(opcode)
+
 
     def list_port(self):
         
@@ -82,6 +143,7 @@ class serial_tool(serial.Serial):
 
         for port, desc, hwid in self.serial_port_list:
             print("{}: {} [{}]".format(port, desc, hwid))
+        return self.serial_port_list
 
     def send_at_cmd(self,cmd,params=None):
         data = ''
@@ -112,14 +174,14 @@ class serial_tool(serial.Serial):
 
     def at_cmd_executor(self,cmd : str,params):
         if cmd == 'A2DPSTAT':
-            if config.target_addr == params[1]:
+            if self.config.target_addr == params[1]:
                 a2dpstat = params[0]
         elif cmd == 'AGSTAT':
             #print(cmd,params)
-            if params[1] == config.target_addr and params[0] == '2':
+            if params[1] == self.config.target_addr and params[0] == '2':
                 self.send_at_cmd('A2DPDISC')
         elif cmd == 'ACLDISC':
-            if params[1] == config.target_addr:
+            if params[1] == self.config.target_addr:
                 #test_count += 1
                 print('acl disc')
                 #self.send_at_cmd('A2DPCONN',config.target_addr)
@@ -134,59 +196,25 @@ class serial_tool(serial.Serial):
         self.at_cmd_executor(cmd,params)   
 
 if __name__ == "__main__":
-    ser_tool = serial_tool()
-    print(ser_tool.baudrate)
     
-    config = parse_config.parse_config()
-    print(config.target_addr)
+    ser_tool = Serial_tool()
+    ser_tool.port = '/dev/ttyUSB0'
+    ser_tool.open_port()
+    
+    # bind 3341 to accept a client to recv cmd
+    ADDR = ('127.0.0.1',3341)
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.bind(ADDR)
+    s.listen()
     while True:
-        opcode = input()
-        if opcode == '1':
-            ser_tool.list_port()
-        elif opcode == '2':
-            ser_tool.open_port()
-        elif opcode == '3':
-            ser_tool.close_port()
-        elif opcode == '4':
-            ser_tool.write('AT+B GPRL\r\n'.encode('utf-8'))
-        elif opcode == '5':
-            ser_tool.send_at_cmd('INQU','2')
-        elif opcode == '6':
-            ser_tool.send_at_cmd('A2DPCONN',config.target_addr)
-        elif opcode == 'CONN':
-            ser_tool.send_at_cmd('A2DPCONN',config.target_addr)
-        elif opcode == 'DISC':
-            ser_tool.send_at_cmd('A2DPDISC')
-        elif opcode == 'START':
-            ser_tool.send_at_cmd('A2DPSTART')
-        elif opcode == 'SUSPEND':
-            ser_tool.send_at_cmd('A2DPSUSPEND')
-        elif opcode == 'CLOSE':
-            ser_tool.send_at_cmd('A2DPCLOSE')
-        elif opcode == 'GLBD':
-            ser_tool.send_at_cmd('GLBD')
-        elif opcode == 'GPRL':
-            ser_tool.send_at_cmd('GPRL')
-        elif opcode == 'DPRD':
-            ser_tool.send_at_cmd('DPRD','00000000000000')
-        elif opcode == 'INQU':
-            ser_tool.send_at_cmd('INQU','2')
-        elif opcode == 'AGCONN':
-            ser_tool.send_at_cmd('AGCONN',config.target_addr)
-        elif opcode == 'AGDISC':    
-            ser_tool.send_at_cmd('AGDISC')
-        
-        time.sleep(0.01)
+        #opcode = prompt('> ', history = FileHistory('/tmp/history.txt'))
+        #opcode = input()
+        sock,addr = s.accept()
+        while True:
+            data = sock.recv(1024)
+            if not data:
+                break
+            data_decode = data.decode('utf-8')
+            ser_tool.hanle_at_cmd_send(data_decode)
 
-        """ time.sleep(5)
-        ser_tool.list_port()
-        if flag == 1:
-            ser_tool.close_port()
-            flag = 0
-            print('close port')
-        else:
-            ser_tool.open_port()
-            flag = 1
-            ser_tool.write('AT+B GPRL\r\n'.encode('utf-8'))
-            print('open port')            
-        print('app heartbeat') """
+        time.sleep(0.01)
